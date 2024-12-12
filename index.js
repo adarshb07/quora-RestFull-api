0e2e2e7e134e716d9136df36b3614212e09f5ab6
Adarsh Bhardwaj <adarshbhardwaj285@gmail.com>
2024-12-12 17:11:34 +0530

    [ADD] index.js: Add View Single Post and Edit Post Using PATCH method

--- index.js
+++ index.js
@@ -3,23 +3,28 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
 	{
+		id: uuidv4(),
 		username: "adarshB",
 		content: "I love Coding",
 	},
 	{
+		id: uuidv4(),
 		username: "testuser",
 		content: "This is Test POST",
 	},
 	{
+		id: uuidv4(),
 		username: "newUser",
 		content: "Hardwork is key for everything",
 	},
@@ -39,8 +44,31 @@ app.get("/posts/new", (req, res) => {
 
 app.post("/posts", (req, res) => {
 	let { username, content } = req.body;
-	posts.push({ username, content });
+	let id = uuidv4();
+	posts.push({ id, username, content });
 	res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+	let { id } = req.params;
+	let post = posts.find((p) => id === p.id);
+	res.render("post.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+	let { id } = req.params;
+	let newContent = req.body.content;
+	let post = posts.find((p) => id === p.id);
+	post.content = newContent;
+	res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+	let { id } = req.params;
+	console.log(id);
+	let post = posts.find((p) => id === p.id);
+	console.log(post);
+	res.render("edit.ejs", { post });
 });
 
 app.listen(port, () => {
